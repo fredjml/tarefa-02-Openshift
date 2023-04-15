f993fd15e90ebeb8055fc3ea399c9c193d38a898
fredjml <127332573+fredjml@users.noreply.github.com>
2023-04-15 20:08:45 -0300

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return '<h1><p style="color:' + os.environ.get('APP_COLOR', 'black') + '">g-Bruno-Erick-Daniel-Fred TrabFinal Pratica2 build-app OpenShift Builds repositório git Webhook</p></h1>'
+    return '<h1><p style="color:' + os.environ.get('APP_COLOR', 'black') + '">g-Bruno-Erick-Daniel-Fred TrabFinal Pratica2 build-app OpenShift Builds repositório git Webhook teste 2</p></h1>'
 
 @app.route('/api')
 def hello():
